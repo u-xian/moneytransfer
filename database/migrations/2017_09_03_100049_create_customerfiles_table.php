0955e807a5197407aeae4873a9f3f35f6f922f02
Christian Uwakristu <uxian554@gmail.com>
2017-09-03 12:07:49 +0200

    Changes on customerfiles table

--- database/migrations/2017_09_03_100049_create_customerfiles_table.php
+++ database/migrations/2017_09_03_100049_create_customerfiles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomerFilesTable extends Migration
+class CreateCustomerfilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCustomerFilesTable extends Migration
      */
     public function up()
     {
-        Schema::create('customerFiles', function (Blueprint $table) {
+        Schema::create('customerfiles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->integer('size');
@@ -30,6 +30,6 @@ class CreateCustomerFilesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('customerFiles');
+        Schema::dropIfExists('customerfiles');
     }
 }
